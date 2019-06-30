d90de98b0fe97d9d8115e00860ceac355798134e
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-30 03:08:11 +0000

    Snapshot of branch master taken on 2019-06-29T23:08:10.682-04:00 from "https://github.com/UnofficialJuliaMirror/JuliaIO-LibTIFFBuilder"

---
+++ build_tarballs.jl
@@ -0,0 +1,40 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libtiff"
+version = v"4.0.9"
+
+# Collection of sources required to build libtiff
+sources = [
+    "https://gitlab.com/libtiff/libtiff/-/archive/Release-v4-0-9/libtiff-Release-v4-0-9.tar.gz" =>
+    "6656637a1183c32227334fc0d0d08e011e3da74804bcdcaec496723a9612f97a",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libtiff*/
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libtiff", :libtiff),
+    LibraryProduct(prefix, "libtiffxx", :libtiffxx)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl",
+    "https://github.com/JuliaIO/LibpngBuilder/releases/download/v1.0.3/build_libpng.v1.6.37.jl",
+    "https://github.com/SimonDanisch/LibJPEGBuilder/releases/download/v10/build_libjpeg.v9.0.0-b.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
